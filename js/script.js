6555a31c62ac6d311b38f36bc9a0e1b1ec089dc3
Steve Bernard <93719219+meijiSB@users.noreply.github.com>
2021-12-03 14:45:04 +0000

    Slide menu on mobile devices
    Adds a slide out menu and interactive hamburger button for mobile device navigation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
-var app = document.getElementById('handwriting');
+//Typewriter effect
+const indexType = document.getElementById('index-type');
 
-var typewriter = new Typewriter(app, {
+let typewriter = new Typewriter(indexType, {
     loop: false,
     delay: 100
 });
 
 typewriter.typeString("I'm a web developer").start();
+
+
+//Mobile menu
+let $menuOpen = false;
+let $mobileWidth = window.matchMedia( "(min-width: 768px)" );
+
+const $toggleMenu = function(){
+    $('.sidebar-hamburger').toggleClass('open');
+    $('.nav-container').toggleClass('open');
+    if ($menuOpen === false)
+        { $menuOpen = true; } else { $menuOpen = false; }
+}
+
+$(document).ready(function(){
+	$('.sidebar-hamburger').click($toggleMenu);
+});
+
+$mobileWidth.addEventListener("change", () => {
+  if ($mobileWidth.matches && $menuOpen) {
+    $toggleMenu();
+  }
+});
+
+$(document).click((event) => {
+    if (!$(event.target).closest('.nav-container').length && $menuOpen) {
+        $toggleMenu();
+    }        
+  });
