7368ac47054f6094ce225162198cd6539dbac027
Steve Bernard <93719219+meijiSB@users.noreply.github.com>
2021-12-05 14:09:25 +0000

    Takes loop outside of window.onload
    Narrowed down issue to window.onload on mobile, tried an alternative approach

--- examples/jSnake/js/jsnake.js
+++ examples/jSnake/js/jsnake.js
@@ -70,65 +70,74 @@ function checkFood() {
   }
 }
 
-window.onload = function() {
 function moveSnake() {
-    if (game != "over")
+  if (game != "over")
+  {
+  doubleClickCatcher = false; //lets you input again
+  let arr = visited.slice(0, score); //all coordinates limited to current length of snake
+
+    if (visited.length > score) { //turns off last checkbox in array
+      let rem = visited[score];
+      document.getElementById(rem).checked = false;
+    }
+
+    if (x<=size && x>=0 && y<=size && y>=0) // if within boundary
     {
-    doubleClickCatcher = false; //lets you input again
-    let arr = visited.slice(0, score); //all coordinates limited to current length of snake
-
-      if (visited.length > score) { //turns off last checkbox in array
-        let rem = visited[score];
-        document.getElementById(rem).checked = false;
+      if (arr.indexOf(x+","+y) > 0) {// if collision
+        game = "over";
+      }else if (document.getElementById(x+","+y).checked === true && visited.length > 1) {// eats a pixel
+        score += 1;
+        document.getElementById("scoreboard").innerHTML = "SCORE: "+score;
+        placeFood();
       }
 
-      if (x<=size && x>=0 && y<=size && y>=0) // if within boundary
-      {
-        if (arr.indexOf(x+","+y) > 0) {// if collision
-          game = "over";
-        }else if (document.getElementById(x+","+y).checked === true && visited.length > 1) {// eats a pixel
-          score += 1;
-          document.getElementById("scoreboard").innerHTML = "SCORE: "+score;
-          placeFood();
-        }
+      document.getElementById(x+","+y).checked = true;
 
-        document.getElementById(x+","+y).checked = true;
-
-        if (visited.at(-1) != x+","+y || visited.at(0) != startingPos) {
-            visited.unshift(x+","+y);
-        }
-      }
-      else { game = "over"; }
-
-      //Move snake
-      if (direction === "right") { y++; }
-      else if (direction === "left") { y--; }
-      else if (direction === "up") { x--;}
-      else if (direction === "down") { x++; }
-
-      //check food square is ticked
-      checkFood();
-
-      //check end conditions
-      if (game === "over" && document.getElementById("scoreboard").innerHTML.startsWith("SCORE")){
-        document.getElementById("scoreboard").innerHTML = 
-        "FINAL SCORE: "+score+"<br><br><a href='javascript:location.reload();'>NEW GAME</a>";
-        console.log("Game over");
+      if (visited.at(-1) != x+","+y || visited.at(0) != startingPos) {
+          visited.unshift(x+","+y);
       }
     }
-  }
-  setInterval(moveSnake, speed);
+    else { game = "over"; }
 
-  document.getElementById("submitLeft").onclick = function () { 
-    direction = "left"; doubleClickCatcher = true; 
-  }
-  document.getElementById("submitRight").onclick = function () { 
-    direction = "right"; doubleClickCatcher = true; 
-  }
-  document.getElementById("submitUp").onclick = function () { 
-    direction = "up"; doubleClickCatcher = true; 
-  }
-  document.getElementById("submitDown").onclick = function () { 
-    direction = "down"; doubleClickCatcher = true; 
+    //Move snake
+    if (direction === "right") { y++; }
+    else if (direction === "left") { y--; }
+    else if (direction === "up") { x--;}
+    else if (direction === "down") { x++; }
+
+    //check food square is ticked
+    checkFood();
+
+    //check end conditions
+    if (game === "over" && document.getElementById("scoreboard").innerHTML.startsWith("SCORE")){
+      document.getElementById("scoreboard").innerHTML = 
+      "FINAL SCORE: "+score+"<br><br><a href='javascript:location.reload();'>NEW GAME</a>";
+      console.log("Game over");
+    }
   }
 }
+
+let ready = function ( fn ) {
+
+  // Sanity check
+  if ( typeof fn !== 'function' ) return;
+
+  // If document is already loaded, run method
+  if ( document.readyState === 'complete'  ) {
+      return fn();
+  }
+
+  // Otherwise, wait until document is loaded
+  document.addEventListener( 'DOMContentLoaded', fn, false );
+
+};
+
+ready(function() {
+    moveSnake();
+    setInterval(moveSnake, speed);
+});
+
+// window.onload = function() {
+//   moveSnake();
+//   setInterval(moveSnake, speed);
+// }
