d14c4ca32f9aa7fca7d88926cb456d19efad5dec
Steve Bernard <93719219+meijiSB@users.noreply.github.com>
2021-12-07 15:55:59 +0000

    Form confirmation
    Added a confirmation is form is submitted successfully and cleared form, some other minor changes

--- js/script.js
+++ js/script.js
@@ -29,13 +29,17 @@ $(document).click((event) => { //close menu when tap outside of it
 
 
 //Contact form validation
-function isEmail(email) {
-  const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-  return regex.test(email);
+function isLetters(value) {
+  const regex = /^[a-zA-Z]+$/;
+  return regex.test(value);
 }
-function isPhone(number) {
+function isEmail(value) {
+  const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  return regex.test(value);
+}
+function isPhone(value) {
   const regex = /^((\+44\s?\d{4}|\(?\d{5}\)?)\s?\d{6})|((\+44\s?|0)7\d{3}\s?\d{6})$/;
-  return regex.test(number);
+  return regex.test(value);
 }
 
 $('.newsletter-signup').submit(function( event ) {
@@ -46,35 +50,49 @@ $('.newsletter-signup').submit(function( event ) {
   let $emailSubject = $('#email-subject').val();
   let $emailMessage = $('#email-message').val();
 
-  if ($emailName === "") {
+  let $failure = 0;
+
+  if ($emailName === "" || !isLetters($emailName)) {
     $("label[for='email-name']").html('<span class="required">*Please provide a <strong>contact name</strong>:</span><br>');
+    $failure++;
   }else{
     $("label[for='email-name']").html('<span class="required"></span>');
   }
   
   if ($emailAddress === "" || !isEmail($emailAddress)) {
     $("label[for='email-address']").html('<span class="required">*Please double check your <strong>email address</strong>:</span><br>');
+    $failure++;
   }else{
     $("label[for='email-address']").html('<span class="required"></span>');
   }
 
   if ($emailNumber != "" && !isPhone($emailNumber)) {
     $("label[for='phone-number']").html('<span class="required">*Please double check your <strong>phone number</strong>:</span><br>');
+    $failure++;
   }else{
     $("label[for='phone-number']").html('<span class="required"></span>');
   }
 
   if ($emailMessage === "") {
     $("label[for='email-message']").html('<span class="required">*Please let me know what you\'d like to <strong>contact me about</strong>:</span><br>');
+    $failure++;
   }else{
     $("label[for='email-message']").html('<span class="required"></span>');
   }
 
+  if ($failure > 0) {
+    $failure = 0; //if any of the fields were incorrect on submit, stop from sending and reset counter
+  }else{
+    alert(`Thanks, your email has been submitted.
+
+Name: ${$emailName}
+Email address: ${$emailAddress}
+Number: ${$emailNumber}
+Subject: ${$emailSubject}
+Message: ${$emailMessage}`);
 
 
-//   alert(`Name: ${$emailName}
-// Address: ${$emailAddress}
-// Number: ${$emailNumber}
-// Subject: ${$emailSubject}
-// Message: ${$emailMessage}`);
+    //clear fields
+    $('.newsletter-signup')[0].reset();
+  }
 });
